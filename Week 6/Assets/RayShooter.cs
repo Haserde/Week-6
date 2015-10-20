fe27c6c9e1bb70c8800f431b5953b571408fa2a3
Hasibe Erdemir <erdeh815@newschool.edu>
2015-10-19 22:38:10 -0400

    huge committtt

---
+++ Week 6/Assets/RayShooter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+	public float rayDistance = 5;
+	
+	// Use this for initialization
+	void Start () {
+		print ("howdy");
+		Debug.Log ("yes this is a greeting");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				RaycastHit hitInfo;
+				if (Physics.Raycast (transform.position, transform.forward, out hitInfo, rayDistance)) {
+					hitInfo.collider.GetComponent<cycleTextures> ().changeTexture ();
+				}
+			}
+		}
+	}
+	void OnDrawGizmos(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay (transform.position, transform.forward * rayDistance);
+	}
+}
---
+++ Week 6/Assets/spriteMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spriteMover : MonoBehaviour {
+
+	Animator spriteController;
+
+	// Use this for initialization
+	void Start () {
+		spriteController = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow) ) { 
+			spriteController.SetInteger ("State", 0);
+		} else if  (Input.GetKey (KeyCode.UpArrow) ){
+			spriteController.SetInteger("State", 1);
+		}
+	}
+}
